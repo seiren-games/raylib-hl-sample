c6cb4dbe5ae6b6f2b2efcc23ebe16ea0e0073160
seiren <seirengames@gmail.com>
2022-12-09 13:41:09 +0900

    update

--- start-vscode-from-vcvars64.bat
+++ start-vscode-from-vcvars64.bat
@@ -1,4 +1,4 @@
 @rem Move location of the batch file itself.
 cd /d %~dp0
-call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+call "C:\Program Files\Microsoft Visual Studio\2022\Community\VC\Auxiliary\Build\vcvars64.bat"
 code .vscode/raylib-hl-sample.code-workspace
